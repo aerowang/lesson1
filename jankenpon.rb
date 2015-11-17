3317020aaeb2b4368b67e329f5dcfa38463b6de8
aerowang <wangtenghong@gmail.com>
2015-11-17 22:04:55 +0800

    jankenpon use function & add score board

--- jankenpon.rb
+++ jankenpon.rb
@@ -1,50 +1,86 @@
 require "pry"
 
 def Welcome()
-  # Welcome banner
   puts "************************************"
   puts "*                                  *"
   puts "*  Let's play Rock Paper Scissors  *"
   puts "*                                  *"
   puts "************************************"
+  puts ""
 end 
 
-Welcome()
+def PlayerInput()
+  gesture = ["P", "R", "S"]
 
-again = "Y"
-until again == "N"
-  puts "Please enter (P/R/S):"
-  # Get user input
-  print "Player> "
-  player = gets.chomp.upcase
-  # Get computer input
-  letter = ["P", "R", "S"]
-  computer = letter[rand(0..2)]
-  puts "Computer> #{computer}"
+  begin
+    puts "Please enter (P/R/S)."
+    print "Player> "
+    player_input = gets.chomp.upcase
+    if gesture.include? (player_input)
+      break
+    end
+  end while !gesture.include? (player_input)
+
+  return player_input
+end
+
+def ComputerInput()
+  gesture = ["P", "R", "S"]
+
+  random_num = rand(0..2)
+  gesture = ["P", "R", "S"]
+  computer_input = gesture[random_num]
+  puts "Computer> #{computer_input}"
   
-  # Judge the result
-  print "Resute: "
+  return computer_input
+end
+
+def Judge(player, computer)
+  print "Result: "
   if(player=="R" && computer=="S") ||
     (player=="P" && computer=="R") ||
     (player=="S" && computer=="P")
       puts "You win;)"
+      return "win"
   elsif(player=="R" && computer=="P") ||
     (player=="P" && computer=="S") ||
     (player=="S" && computer=="R")
       puts "You lose:("
+      return "lose"
   else
       puts "Deuce"
+      return "deuce"
   end
+end
 
-  # Ask player want to play again?
+def Ending(score)
+  puts "    Total:"
+  puts "           Win: #{score["win"]}"
+  puts "          Lose: #{score["lose"]}"
+  puts "         Deuce: #{score["deuce"]}"
+  puts ""
+  puts "************************************"
+  puts "*                                  *"
+  puts "*         Have a nice day!         *"
+  puts "*                                  *"
+  puts "************************************"
+end
+
+
+Welcome()
+score = {"win"=>0, "lose"=>0, "deuce"=>0}
+again = "Y"
+until again == "N"
+  player_input = PlayerInput()
+  computer_input = ComputerInput()
+  result = Judge(player_input, computer_input)
+  score[result] += 1
+
   puts "\nDo you want to play again? (Y/N)"
   print "> "
   again = gets.chomp.upcase
-  puts "\n************************************"
-  if again == "N"
-    break
-  end
-  # binding.pry
+  puts "************************************"
 end
 
-puts "\nHave a nice day!\n\n"
+Ending(score)
+# binding.pry
